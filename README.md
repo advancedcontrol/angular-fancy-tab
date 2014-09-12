cf4a3847ddc4f81592affce9f98da4a4e53872b8
Stephen von Takach <steve@cotag.me>
2014-09-12 22:26:19 +1000

    init commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Angular Fancy Tab
+
+
+![screen shot of demo](https://cloud.githubusercontent.com/assets/368013/4249495/9aa07134-3a77-11e4-9ab1-c7544c8a3ad3.png)
+
+
+## Installation
+
+1. Open bower.json
+2. Add `"angular-fancy-tab": "latest"` to your dependency list
+3. Run `bower install`
+4. In your application you can now add:
+   * `<script src="bower_components/angular-fancy-tab/fancy-tab.js"></script>`
+5. Import the SCSS files into your SCSS using the `@import` directive i.e.
+   * `@import "../../bower_components/angular-fancy-tab/fancy-tab.scss"`
+6. Include the CSS using the mixin
+   * `@include fancy-tabs(options: opt-value, ..)`
+
+
+## Usage
+
+See the demo file
---
+++ fancy-tab.js
@@ -0,0 +1,98 @@
+(function (angular) {
+    'use strict';
+
+
+    // Mix in to our module
+    var module;
+    try {
+        module = angular.module('coUtils');
+    } catch (e) {
+        module = angular.module('coUtils', []);
+    }
+
+
+    module
+        .directive('tabs', function() {
+            return {
+                restrict: 'E',
+                transclude: true,
+                scope: {
+                    current: '=?'
+                },
+                controller: ['$scope', '$element', function($scope, $element) {
+                    var panes = $scope.panes = [],
+                        selected;
+
+                    $scope.select = function(pane) {
+                        selected = pane;
+                        angular.forEach(panes, function(pane) {
+                            pane.selected = false;
+                        });
+                        pane.selected = true;
+                        $scope.current = selected.name || selected.title;
+                    }
+
+                    this.addPane = function(pane) {
+                        if (panes.length == 0)
+                            $scope.select(pane);
+
+                        panes.push(pane);
+                    }
+
+                    // Allow for programmatic tab selection
+                    $scope.$watch('current', function (val) {
+                        if (val !== (selected.name || selected.title)) {
+                            angular.forEach(panes, function(pane) {
+                                if (val === (selected.name || selected.title)) {
+                                    pane.selected = true;
+                                } else {
+                                    pane.selected = false;
+                                }
+                            });
+                        }
+                    });
+                }],
+                template:
+                    '<div>' +
+                        '<nav>' +
+                            '<div ng-repeat="pane in panes" ng-if="!pane.hide" ng-class="{active: pane.selected}" ng-click="select(pane)">' +
+                                '<div class="{{pane.icon}}"></div>' +
+                                '<span>{{pane.title}}</span>' +
+                            '</div>' +
+                        '</nav>' +
+                        '<div class="tab-content" ng-transclude></div>' +
+                    '</div>'
+            };
+        })
+
+        .directive('pane', function() {
+            return {
+                require: '^tabs',
+                restrict: 'E',
+                transclude: true,
+                scope: {
+                    title: '@',
+                    icon: '@',
+                    selected: '=?',
+                    name: '=?',
+                    hide: '=?'
+                },
+                link: function(scope, element, attrs, tabsCtrl) {
+                    scope.hide = scope.hide || false;
+
+                    tabsCtrl.addPane(scope);
+
+                    scope.$watch('selected', function (val) {
+                        if (val) {
+                            element.addClass('active');
+                        } else {
+                            element.removeClass('active');
+                        }
+                    });
+                },
+                template:
+                    '<div ng-if="selected" ng-transclude></div>'
+            };
+        });
+
+}(this.angular));
